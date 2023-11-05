19e3087306924d53f72a0db9f185267b84233ccf
maksim <mkotelnikov97@gmail.com>
2023-11-05 21:58:41 +0300

    Добавил отображение загрузки, показ картинок. (есть пару багов)

--- weather/src/components/Weather.tsx
+++ weather/src/components/Weather.tsx
@@ -1,6 +1,8 @@
 import sun from '../../public/images/sun.png';
 import rain from '../../public/images/rain.png';
 import fog from '../../public/images/fog.png';
+import cloud from '../../public/images/cloud.png';
+import thunderstorm from '../../public/images/Thunderstorms.png';
 
 import search from '../../public/images/search.svg';
 import maps from '../../public/images/maps.png';
@@ -11,69 +13,110 @@ import { ChangeEvent } from 'react';
 const Weather = () => {
     const [initCityName, setInitCityName] = useState('');
     const [nameCity, setNameCity] = useState('');
+    const [loading, setLoading] = useState(false);
     const [data, setData] = useState({
         celcius: '',
         weatherStatus: ''
     });
-    console.log(data);
+
     const initCity = (e: ChangeEvent<HTMLInputElement>) => {
         setInitCityName(e.target.value);
     };
 
     const selectCity = () => {
+        if (initCityName.length === 0 || initCityName.includes(' '))
+            alert('Некорректно введены данные');
         setNameCity(initCityName);
-        setInitCityName('')
+        setInitCityName('');
+        setLoading(true);
     };
 
     const WEATHER_API: string = `https://api.openweathermap.org/data/2.5/weather?q=${nameCity}&appid=16bfa98849718de13b6e8978b87d47b8`;
 
     useEffect(() => {
-            try {
-                axios.get(WEATHER_API)
-                    .then(res => {
-                        const celciusTemp = (res.data.main.temp - 273.15).toFixed(0);
-                        const temperatureWithSign = parseFloat(celciusTemp) >= 0 ? `+${celciusTemp}°` : `${celciusTemp}°`;
-                        setData({ ...data, celcius: temperatureWithSign, weatherStatus: res.data.weather[0].main });
-                        console.log(data.weatherStatus);
-                    });
-            } catch (e) {
-                console.error(`error: ${e}`);
-            }
-        },
-        [nameCity]);
-
+        try {
+            axios.get(WEATHER_API).then(res => {
+                const celciusTemp = (res.data.main.temp - 273.15).toFixed(0);
+                const temperatureWithSign =
+                    parseFloat(celciusTemp) >= 0
+                        ? `${celciusTemp}°`
+                        : `${celciusTemp}°`;
+                setData({
+                    ...data,
+                    celcius: temperatureWithSign,
+                    weatherStatus: res.data.weather[0].main
+                });
+                setLoading(false);
+            });
+        } catch (e) {
+            console.error(`error: ${e}`);
+        }
+    }, [nameCity]);
 
     return (
-        <div
-            className='rounded-lg mt-24 bg-cover pb-5 pt-5 text-amber-50'>
+        <div className='rounded-lg mt-24 bg-cover pb-5 pt-5 text-amber-50'>
             <section className=''>
-                <div
-                    className='rounded-lg text-center bg-white/20 backdrop-blur-lg backdrop-opacity-50 mx-auto w-1/4'
-                >
+                <div className='rounded-lg text-center bg-white/20 backdrop-blur-lg backdrop-opacity-50 mx-auto w-1/4'>
                     <div className='mx-auto'>
-                        <input type='text'
-                               className='pl-10 mr-5 mt-10 rounded-lg p-1.5 bg-white/20 bg-opacity-50 outline-none'
-                               placeholder='Найти свой город...'
-                               onChange={initCity}
-                               value={initCityName}
+                        <input
+                            type='text'
+                            className='pl-10 mr-5 mt-10 rounded-lg p-1.5 bg-white/20 bg-opacity-50 outline-none'
+                            placeholder='Найти свой город...'
+                            onChange={initCity}
+                            value={initCityName}
                         />
                         <button
                             className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-5 px-10 rounded-full transition active:bg-cyan-950'
-                            onClick={selectCity}>
-                            <img src={search} alt='search' width='40' className='absolute top-14 right-14 w-6 h-6' />
+                            onClick={selectCity}
+                        >
+                            <img
+                                src={search}
+                                alt='search'
+                                width='40'
+                                className='absolute top-14 right-14 w-6 h-6'
+                            />
                         </button>
-
-                        <img src={
-                            data.weatherStatus === 'Clear' ? sun : data.weatherStatus === 'rain' ? rain : data.weatherStatus === ('clouds') ? fog : ''
-                        } alt='weather-icon' width='230' className='mr-auto ml-auto mb-10 mt-10' />
-                        <h1 className='text-5xl'>{data.celcius}</h1>
-                        <div className='flex text-center justify-center mt-5 pb-10'>
-                            {nameCity !== '' ? <img src={maps} alt='map' width='25' /> : ''}
-                            <h3 className='text-1xl my-auto'>{nameCity}</h3>
-                        </div>
+                        {loading ? (
+                            <p>Загрузка...</p>
+                        ) : (
+                            <div>
+                                {nameCity && (
+                                    <img
+                                        src={
+                                            data.weatherStatus === 'Clear'
+                                                ? sun
+                                                : data.weatherStatus === 'Rain'
+                                                ? rain
+                                                : data.weatherStatus ===
+                                                  'Clouds'
+                                                ? cloud
+                                                : data.weatherStatus ===
+                                                  'Thunderstorm'
+                                                ? thunderstorm
+                                                : data.weatherStatus === 'Smoke'
+                                                ? fog
+                                                : ''
+                                        }
+                                        alt='weather-icon'
+                                        width='230'
+                                        className='mr-auto ml-auto mb-10 mt-10'
+                                    />
+                                )}
+                                <h1 className='text-5xl'>{data.celcius}</h1>
+                                <div className='flex text-center justify-center mt-5 pb-10'>
+                                    {nameCity !== '' ? (
+                                        <img src={maps} alt='map' width='25' />
+                                    ) : (
+                                        ''
+                                    )}
+                                    <h3 className='text-1xl my-auto'>
+                                        {nameCity}
+                                    </h3>
+                                </div>
+                            </div>
+                        )}
                     </div>
                 </div>
-
             </section>
         </div>
     );
